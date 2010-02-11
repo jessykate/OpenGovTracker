37b7b255c49eae3e2c7392a7f2b8c7331b734adf
jessykate <jessy.cowansharp@gmail.com>
2010-02-11 10:33:22 -0500

    typo in settings file

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 settings = {
-    "static_path": os.path.join(os.path.dirname(__file__), "static")
+    "static_path": os.path.join(os.path.dirname(__file__), "static"),
     "stats_cache": "/tmp/stats_cache.json",
 }
