34dc5170f58c469df6ed3b4a8cb73c686653eae2
jessykate <jessy.cowansharp@gmail.com>
2010-02-13 16:42:42 -0500

    added cache directory

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ import os
 
 settings = {
     "static_path": os.path.join(os.path.dirname(__file__), "static"),
-    "stats_cache": "/tmp/stats_cache.json",
+    "stats_cache": "cache/stats_cache.json",
 }
