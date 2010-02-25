75bc4343e046aceda649b02557ec2ccb2088e0be
jessykate <jessy.cowansharp@gmail.com>
2010-02-24 19:53:46 -0500

    added agency reports. still needs pie chart for categories

--- static/js/ogt.js
+++ static/js/ogt.js
@@ -11,21 +11,32 @@ $(document).ready(function() {
 function toggle_visible(agency_link) {
   // get the link to the agency we want to make visible
   var agency = agency_link.getAttribute("href");
-  agency_link.style["font-weight"] = "bold";
 
-  // strip the leading #
+  // strip the leading # in the href value
   var visible_agency = agency.slice(1,agency.length);
 
+  /* get only the child *elements* of the reports section. don't use
+   getElementsByTagName() because it will grab all nested div's, too,
+   which will screw up the display toggling. */
   var report_section = document.getElementById("reports");
-  var reports = report_section.getElementsByTagName("div");
+  var report_children = report_section.childNodes;
+  var reports = Array();
+  for (var i=0; i<report_children.length; i++) {
+    if (report_children[i].nodeType == 1) {
+      reports.push(report_children[i]);
+    }
+  }
+
+  // make the selected agency report visible, suppressing the rest
   for (var i=0; i< reports.length; i++) {
     var link = reports[i].getElementsByTagName("a")[0];
     var agency_name = link.getAttribute("name");
     if (agency_name == visible_agency) {
       reports[i].style.display="";
+      link.style.fontWeight = "bold";
     }
     else {
-      link.style["font-weight"] = "normal";
+      link.style.fontWeight = "none";
       reports[i].style.display="none";
     }
   }
@@ -42,11 +53,20 @@ window.onload = function() {
     };
   }
 
-  // disable display of each agency's report to start
-  var reports_section = document.getElementById("reports");
-  var agency_sections = reports_section.getElementsByTagName("div");
-  for (var i=0; i< agency_sections.length; i++) {
-    agency_sections[i].style.display="none";
+  /* show the summary table but disable display of agency-specific
+   * reports to start */
+  var report_section = document.getElementById("reports");
+  var report_children = report_section.childNodes;
+  var reports = Array();
+  for (var i=0; i<report_children.length; i++) {
+    if (report_children[i].nodeType == 1) {
+      reports.push(report_children[i]);
+    }
+  }
+  for (var i=0; i< reports.length; i++) {
+    if (reports[i].getAttribute("id") != "summary_table") {
+      reports[i].style.display="none";
+    }
   }
 };
 
